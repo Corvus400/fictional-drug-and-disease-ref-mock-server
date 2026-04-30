8c2694c9ad2c3934ec4bb659300511ebb87ab17f
Corvus400 <13657682+Corvus400@users.noreply.github.com>
2026-05-01 02:09:32 +0900

    test: drive original image resize behavior

--- src/main/kotlin/io/github/corvus400/fictionaldrugdiseaserefmockserver/routes/common/ImageResizer.kt
+++ src/main/kotlin/io/github/corvus400/fictionaldrugdiseaserefmockserver/routes/common/ImageResizer.kt
@@ -1,7 +1,16 @@
 package io.github.corvus400.fictionaldrugdiseaserefmockserver.routes.common
 
+import java.awt.image.BufferedImage
+
 enum class ImageSize {
     S,
     M,
     ORIGINAL,
+}
+
+object ImageResizer {
+    fun resize(
+        originalImage: BufferedImage,
+        size: ImageSize,
+    ): BufferedImage = originalImage
 }
